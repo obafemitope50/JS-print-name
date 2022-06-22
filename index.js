1684b4dc08c992727771f9915b388439d672cf30
obafemitope50 <obafemitope50@gmail.com>
2022-06-22 05:35:14 -0700

    This is a simple print name program

---
+++ index.js
@@ -0,0 +1,14 @@
+let myName = 'Temitope';
+let height = '187cm';
+let country = 'Nigeria';
+
+let body = document.querySelector('body');
+let content = 'Name:  '+ myName + ', Height: ' + height + ', Country: ' +country;
+
+body.innerHTML = content;
+
+
+            //OR using alert
+
+alert('Name: Temitope, Height: 187cm, Country: Nigeria')
+
